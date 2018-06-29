b7166d5a3b9c5338d0149de09eaf86fc557c1daa
Je <lokargenia@gmail.com>
2018-06-29 11:00:42 +0300

    Update algorithm.java
    Изменения связанные со String name

--- algorithm.java
+++ algorithm.java
@@ -2,16 +2,17 @@ public class algorithm {
     /*Основная цель - работа с графом*/
     public static void main(String[] args) {
         graph NewGraph = new graph();
-        NewGraph.AddNode(1);
-        NewGraph.AddNode(2);
-        NewGraph.AddNode(3);
-        
-        NewGraph.CreateRelat(1,2);
-        NewGraph.CreateRelat(3,1);
-        
+        NewGraph.AddNode("1");
+        NewGraph.AddNode("2");
+        NewGraph.AddNode("3");
+
+        NewGraph.CreateRelat("1","2");
+        NewGraph.CreateRelat("3","1");
+
         NewGraph.ShowGraph();
         DFS start = new DFS(NewGraph);
         //start.ShowRes();
     }
 
 }
+
