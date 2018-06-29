d35fea28a5f3762799d14737ffdfcdb47f190eed
Je <lokargenia@gmail.com>
2018-06-29 04:05:39 +0300

    Update DFS.java
    Изменён Алгоритм поиска в глубину. Вроде бы работате коректно.
    Вывод на экран ребро(Вершина 1, Вершина 2), которое является мостом
    Внесены комментарии

--- DFS.java
+++ DFS.java
@@ -1,60 +1,54 @@
 public class DFS {
     private graph NewGraph ;
-    private int time = -1;
+
+    private int time = 0;
     private int S[] ;
     private int Up[] ;
     private boolean Res[] ;
+
     public DFS(graph A)
     {
         NewGraph = A;
-        S = new int[NewGraph.GetNumNodes()];
-        Up = new int[NewGraph.GetNumNodes()];
-        Res = new boolean[NewGraph.GetNumNodes()];
+
+        S = new int[NewGraph.GetNumNodes()]; // Start
+        Up = new int[NewGraph.GetNumNodes()]; // End
+        Res = new boolean[NewGraph.GetNumNodes()]; // Most or no?
+
+
         for(int i = 0;i < NewGraph.GetNumNodes();i++)
         {
             if (S[i] == 0)
             {
-                FindMost(0,-1);
+                FindMost(i,0);
             }
         }
-
     }
-    public void FindMost(int v,int p)
-    {
 
+    public void FindMost(int v,int p)// k - номер/номер в списке
+    {
         Res[v] = true;
         S[v] = Up[v] = time++;
-        for(node elem: NewGraph.GetNodeOfIndex(v).GetList())
-        {
-            int to = elem.GetName() - 1;
-            if(to == p) continue;
-            if (Res[to])
-            {
-                Up[v] = Math.min(Up[v],S[to]);
 
+        for(node elem: NewGraph.GetNodeOfIndex(v).GetList()) {
+
+            int to = NewGraph.GetPosOfNode(elem);
+            if (to == p) continue;
+            if (Res[to]) {
+                Up[v] = Math.min(Up[v], S[to]);
+
+            } else {
+                FindMost(to, v);
+                Up[v] = Math.min(Up[v], Up[to]);
+                if (Up[to] > S[v]) {
+                    System.out.print(NewGraph.GetNodeOfIndex(v).GetName());
+                    System.out.print(' ');
+                    System.out.println(NewGraph.GetNodeOfIndex(to).GetName());
+                }
             }
-            else
-            {
-               FindMost(to,v);
-               Up[v] = Math.min(Up[v],Up[to]);
-               if (Up[to] > S[v])
-               {
-                   System.out.print(v+1);
-                   System.out.print(' ');
-                   System.out.println(to+1);
-               }
-            }
-
-
         }
     }
     void ShowRes()
     {
-        for(int i = 0;i < Res.length;i++){
-            if (Res[i] == true)
-            {
-                System.out.println(i+1);
-            }
-        }
+
     }
 }
