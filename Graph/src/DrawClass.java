28c87ed39c7d3e0394e94c90d853b418816ffbc9
Je <lokargenia@gmail.com>
2018-06-30 22:59:13 +0300

    Update DrawClass.java
    Draww Exlips

--- Graph/src/DrawClass.java
+++ Graph/src/DrawClass.java
@@ -1,3 +1,5 @@
+
+import Algorithmpcg.*;
 import Graphpcg.*;
 
 import javax.swing.JFrame;
@@ -22,34 +24,67 @@ public class DrawClass extends JFrame {
         graph.getModel().beginUpdate();
         ArrayList<SimpleNode> nodes = NodesGraph.getNodes();
         int i = 1;
+
+
+
+        //value for rotation
+        double k = 360.0 /nodes.size();
+        double PI = 3.14;
+
+        double Rot = 0;
+
+        double x = 300; // координаты центра во кргу которого рисуем
+        double y = 250;
+
+        double x0 = 300;// координаты первой вершины
+        double y0 = 50;
+
+        double NX;
+        double NY;
+
+
+
         try
-    {
-        for(SimpleNode node: nodes) {
+        {
 
-            Object obj = graph.insertVertex(parent, null, node.getName(),
-                    (rnd.nextInt(500 - 100 + 1)),
-                    (rnd.nextInt(500 - 100 + 1)),
-                    60,60,"shape=ellipse;perimeter=ellipsePerimeter");
-            node.setPictrue(obj);
-            ++i;
-        }
-        for(SimpleNode node: nodes) {
+            for(SimpleNode node: nodes) {
 
-            for(Edge edge: node.getEdgesList()) {
 
-                if(node.equals(edge.getSourceNode()))
-                    graph.insertEdge(parent, null, "", node.getPictrue(),edge.getDestNode().getPictrue());
+                Rot +=k;
+                NX = x + (x0 - x) * Math.cos(Rot * PI / 180) - (y0 - y) * Math.sin(Rot * PI / 180);
+                NY = y + (y0 - y) * Math.cos(Rot * PI / 180) + (x0 - x) * Math.sin(Rot * PI / 180);
+
+
+                Object obj = graph.insertVertex(parent, null, node.getName(),
+                        (NX),
+                        (NY),
+                        60,60,"shape=ellipse;perimeter=ellipsePerimeter");
+                node.setPictrue(obj);
+                ++i;
+            }
+            for(SimpleNode node: nodes) {
+
+                for(Edge edge: node.getEdgesList()) {
+
+                    if(node.equals(edge.getSourceNode()))
+                        graph.insertEdge(parent, null, "", node.getPictrue(),edge.getDestNode().getPictrue());
+                }
             }
         }
-    }
         finally
-    {
-        graph.getModel().endUpdate();
-    }
+        {
+            graph.getModel().endUpdate();
+        }
 
-    mxGraphComponent graphComponent = new mxGraphComponent(graph);
-    getContentPane().add(graphComponent);
-}
+        mxGraphComponent graphComponent = new mxGraphComponent(graph);
+        getContentPane().add(graphComponent);
+
+
+        /**/
+        FindBridge a = new FindBridge(NodesGraph);
+        a.FindBridges();
+        /**/
+    }
 
     public static void main(String[] args)
     {
@@ -58,8 +93,14 @@ public class DrawClass extends JFrame {
         NodeGraph.addNode("First");
         NodeGraph.addNode("Second");
         NodeGraph.addNode("Third");
+        NodeGraph.addNode("5");
+        NodeGraph.addNode("1");
+        NodeGraph.addNode("2");
+        NodeGraph.addNode("3");
         NodeGraph.ConnectNodes("First","Second");
+        NodeGraph.ConnectNodes("5","Second");
         NodeGraph.ConnectNodes("First","Third");
+        NodeGraph.ConnectNodes("Third","5");
         DrawClass frame = new DrawClass(NodeGraph);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1200, 800);
