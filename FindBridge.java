d89deed1817f006fa5bcf73f3f0fc049ec3dacc4
Je <lokargenia@gmail.com>
2018-06-30 21:27:28 +0300

    Update FindBridge.java
    Изменение подбора смежной вершины

--- FindBridge.java
+++ FindBridge.java
@@ -30,6 +30,32 @@ public class FindBridge extends Algorithm{
         Res = new boolean[kolNodes]; // Most or no
 
 
+        /*
+        for(int i = 0; i < kolNodes;i++)
+        {
+            System.out.print(nodes.get(i).getName());
+            System.out.print(": ");
+            for(Edge TMP: nodes.get(i).getEdgesList())
+            {
+                if(nodes.get(i) != TMP.getDestNode())
+                {
+                    System.out.print(TMP.getDestNode().getName());
+                }
+                else
+                {
+                    System.out.print(TMP.getSourceNode().getName());
+                }
+
+                System.out.print(' ');
+            }
+            System.out.println();
+        }
+
+
+        System.out.println();
+        */
+
+
         for(int i = 0;i < kolNodes;i++)
         {
             if (S[i] == 0)
@@ -41,6 +67,9 @@ public class FindBridge extends Algorithm{
 
     public void DPS(int v,int p)
     {
+
+
+
         Res[v] = true;
         S[v] = Up[v] = time++;
 
@@ -49,7 +78,15 @@ public class FindBridge extends Algorithm{
 
         SimpleNode elem;
         for(Edge TMP: nodes.get(v).getEdgesList()) { //pass through the list of adjacent vertices
-            elem= TMP.getDestNode();
+            //elem= TMP.getDestNode();
+            if(nodes.get(v) != TMP.getDestNode())
+            {
+                elem = TMP.getDestNode();
+            }
+            else
+            {
+                elem = TMP.getSourceNode();
+            }
             //int to = NewGraph.GetPosOfNode(elem);
             int to = nodes.indexOf(elem);
             if (to == p) continue;
@@ -60,13 +97,9 @@ public class FindBridge extends Algorithm{
                 DPS(to, v);
                 Up[v] = Math.min(Up[v], Up[to]);
                 if (Up[to] > S[v]) {
-                
-                   /*OUTPUT*/
                     System.out.print(nodes.get(v).getName());
                     System.out.print(' ');
                     System.out.println(nodes.get(to).getName());
-                   /**/ 
-                    
                 }
             }
         }
