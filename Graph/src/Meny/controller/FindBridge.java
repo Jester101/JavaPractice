d2cd168d8ecf001bd58951221f40850a5a1e20cc
Je <lokargenia@gmail.com>
2018-07-02 23:27:02 +0300

    Update FindBridge.java

--- Graph/src/Meny/controller/FindBridge.java
+++ Graph/src/Meny/controller/FindBridge.java
@@ -1,11 +1,16 @@
 package Meny.controller;
-import Meny.model.Edge;
 import Meny.model.*;
 
+import Meny.view.DrawEdge;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class FindBridge extends Algorithm {
 
+
+    private int FlagHag;
+    private int FlagBridge = 0;
     /*value required for the algorithm*/
     private int time = 0;
     private int S[] ;
@@ -15,15 +20,25 @@ public class FindBridge extends Algorithm {
 
     private ArrayList<SimpleNode> nodes ;
     private ArrayList<Edge> edgesOfBridge;
+    private ArrayList<Edge> HagEdge;
+
+
 
     public FindBridge(Graph N)
     {
+
         SetGraph(N);
         nodes = N.getNodes();
         edgesOfBridge = new ArrayList<>() ;
+        HagEdge = new ArrayList<>();
     }
 
 
+
+    public ArrayList<Edge>GetHagArray()
+    {
+        return HagEdge;
+    }
     public ArrayList<Edge> FindBridges()
     {
         int kolNodes = GetGraph().getNodes().size();//bred
@@ -36,17 +51,16 @@ public class FindBridge extends Algorithm {
         {
             if (S[i] == 0)
             {
+
                 DPS(i,0);
             }
         }
+
         return edgesOfBridge;
     }
 
     public void DPS(int v,int p)
     {
-
-
-
         Res[v] = true;
         S[v] = Up[v] = time++;
 
@@ -68,30 +82,23 @@ public class FindBridge extends Algorithm {
                 Up[v] = Math.min(Up[v], S[to]);
 
             } else {
+                HagEdge.add(TMP);
                 DPS(to, v);
+                HagEdge.add(TMP);
                 Up[v] = Math.min(Up[v], Up[to]);
                 if (Up[to] > S[v]) {
 
-                    for(Edge elemEdge:nodes.get(v).getEdgesList())
-                    {
-                        if (nodes.get(v) == elemEdge.getDestNode())
-                        {
-                            if (nodes.get(to) == elemEdge.getSourceNode())
-                            {
+                    for(Edge elemEdge:nodes.get(v).getEdgesList()) {
+                        if (nodes.get(v) == elemEdge.getDestNode()) {
+                            if (nodes.get(to) == elemEdge.getSourceNode()) {
+                                edgesOfBridge.add(elemEdge);
+                            }
+                        } else {
+                            if (nodes.get(to) == elemEdge.getDestNode()) {
                                 edgesOfBridge.add(elemEdge);
                             }
                         }
-
-                        else
-                        {
-                            if (nodes.get(to) == elemEdge.getDestNode())
-                            {
-                                edgesOfBridge.add(elemEdge);
-                            }
-                        }
-                    }
-                }
-            }
-        }
+                    }    }
+        }}
     }
 }
